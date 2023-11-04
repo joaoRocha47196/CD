70ec9c2de67753ba2feaf3611fd6b978dc7a59c7
joaoRocha47196 <joaocrocha2001@gmail.com>
2023-11-04 20:08:08 +0000

    T1 contracts, servers and client definitons

---
+++ 03_trabs/gRPCCalc/ClientApp/src/main/java/clientapp/StreamObservers/RegisterServer.java
@@ -0,0 +1,27 @@
+package clientapp.StreamObservers;
+
+import io.grpc.stub.StreamObserver;
+
+public class RegisterServer implements StreamObserver<ServerEndpoint> {
+
+    @Override
+    public void onNext(ServerEndpoint serverEndpoint) {
+        System.out.println("╔════════════════════════════════════╗");
+        System.out.println("║        Server Endpoint Info        ║");
+        System.out.println("║------------------------------------║");
+        System.out.println("║ Server IP: " + serverEndpoint.getServerIp());
+        System.out.println("║ Server Port: " + serverEndpoint.getServerPort());
+        System.out.println("║------------------------------------║");
+        System.out.println("╚════════════════════════════════════╝");
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        System.err.println("Error: " + t.getMessage());
+    }
+
+    @Override
+    public void onCompleted() {
+        System.out.println("Server endpoint retrieval completed.");
+    }
+}
